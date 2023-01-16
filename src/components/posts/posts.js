84fbfe0260cd42839f2b221f0f3b1ee1ad305708
serdargoleli <web.serdargoleli@gmail.com>
2023-01-16 15:36:52 +0300

    update

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -28,7 +28,6 @@ export default function Posts() {
     });
     return data.length;
   };
-  getCountPost(1);
 
   return (
     <Row>
--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -1,14 +1,13 @@
 import * as ActionsTypes from "../actionsTypes";
 
 const users = [];
-let userAvatar = "";
 
 const usersReducer = (state = users, action) => {
   switch (action.type) {
     case ActionsTypes.SET_USERS:
       return action.payload;
     case ActionsTypes.SET_USER_AVATAR:
-      return (userAvatar = action.payload);
+      return action.payload;
     default:
       return state;
   }
